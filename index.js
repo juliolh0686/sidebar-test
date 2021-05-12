85592bcb0a87490ffb24262c3222ae122d64f262
Julio <juliolh06@gmail.com>
2021-05-12 18:15:49 -0500

    Segunda Actualización

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let closeButton = document.querySelector('.button-close');
 let menuButton = document.querySelector('.button-menu');
 let sidebar = document.querySelector('.sidebar');
 
+function toggleSideBar(){
+    sidebar.classList.toggle('sidebar--active');
+}
+
 closeButton.addEventListener('click',toggleSideBar);
 
 menuButton.addEventListener('click',toggleSideBar);
 
-function toggleSideBar(){
-    sidebar.classList.toggle('sidebar--active');
-}
